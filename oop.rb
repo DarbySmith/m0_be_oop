b46c20bcbc773dbccf858b15813e388f9ae60b39
Darby Smith <newman.darby2@gmail.com>
2022-08-05 21:06:52 -0400

    Add code

--- oop.rb
+++ oop.rb
@@ -1,30 +1,175 @@
-# Write a class called Unicorn
-# it should have a dynamic name attribute
-# it should have a color attribute, that is silver by default
-# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+# # Write a class called Unicorn
+# # it should have a dynamic name attribute
+# # it should have a color attribute, that is silver by default
+# # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+#
+# class Unicorn
+#   def initialize(name)
+#     @name = name
+#     @color = "silver"
+#   end
+#
+#   def change_say(new_say)
+#     puts "*~*#{new_say}~*~"
+#   end
+# end
+#
+# uni1 = Unicorn.new("Cutie Pie")
+# uni1.change_say("hello")
+# p uni1
+#
+#
+# #  Write a class called Vampire
+# #  it should have a dynamic name attribute
+# #  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
+# #  it should have a thirsty attribute, that is true by default
+# #  it should have a drink method. When called, the thirsty attribute changes to false
+#
+# class Vampire
+#   attr_reader :pet
+#   def initialize(name, pet: "bat")
+#     @name = name
+#     @pet = pet
+#     @thirsty = true
+#   end
+#
+#   def drink()
+#     @thirsty = false
+#   end
+# end
+#
+# vam1 = Vampire.new("Howie", :pet => 'dog')
+# p vam1
+#
+# vam1.drink()
+# p vam1
+#
 
+# #  Write a Dragon class
+# #  it should have a dynamic name attribute (string)
+# #  it should have a dynamic rider attribute (string)
+# #  it should have a dynamic color attribute (string)
+# #  it should have a is_hungry attribute that is true by default
+# #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+#
+# class Dragon
+#   def initialize(name, rider, color)
+#     @name = name
+#     @rider = rider
+#     @color = color
+#     @is_hungry = true
+#     @eats = 0
+#   end
+#
+#   def eat()
+#     @eats +=1
+#     if @eats >= 4
+#       @is_hungry = false
+#       puts @is_hungry
+#     end
+#   end
+# end
+#
+# drag1 = Dragon.new("a", "b", "c")
+# drag1.eat()
+# p drag1
+#
+# drag1.eat()
+# drag1.eat()
+# drag1.eat()
+# drag1.eat()
+# p drag1
+#
 
+# #  Write a Hobbit class
+# #  it should have a dynamic name attribute (string)
+# #  it should have a dynamic disposition attribute (string)
+# #  it should have an age attribute that defaults to 0
+# #  it should have a celebrate_birthday method. When called, the age increases by 1
+# #  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
+# #  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
+# #  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+#
+class Hobbit
+attr_reader :is_adult, :is_old, :has_ring
 
-#  Write a class called Vampire
-#  it should have a dynamic name attribute
-#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
-#  it should have a thirsty attribute, that is true by default
-#  it should have a drink method. When called, the thirsty attribute changes to false
+  def initialize(name, disposition, is_adult: false, is_old: false, has_ring: false)
+    @name = name
+    @disposition = disposition
+    @age = 0
+    @is_adult = is_adult
+    @is_old = is_old
+    @has_ring = has_ring
+  end
 
+  def celebrate_birthday
+    @age +=1
+    if @age >= 33 && @age >= 101
+      puts @is_adult = true, @is_old = true
+    elsif @age >= 33 && @age <101
+      puts @is_adult = true
+    end
+  end
 
-#  Write a Dragon class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic rider attribute (string)
-#  it should have a dynamic color attribute (string)
-#  it should have a is_hungry attribute that is true by default
-#  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+  def name
+    if @name = "Frodo"
+      puts @has_ring = true
+    else
+      puts @has_ring = false
+    end
+  end
+end
 
+hob1 = Hobbit.new("Frodo", "greedy")
+p hob1
 
-#  Write a Hobbit class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic disposition attribute (string)
-#  it should have an age attribute that defaults to 0
-#  it should have a celebrate_birthday method. When called, the age increases by 1
-#  it should have an is_adult attribute (boolean) that is false by default. once a Hobbit is 33, it should be an adult
-#  it should have an is_old attribute that defaults to false. once a Hobbit is 101, it is old.
-#  it should have a has_ring attribute. If the Hobbit's name is "Frodo", true, if not, false.
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+hob1.celebrate_birthday
+
+p hob1
